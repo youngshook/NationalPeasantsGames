1bfe26e62b0106238bc48c28c46c5a49743d6a27
Young Shook <pumianwuya@163.com>
2012-05-16 22:37:02 +0800

    Add "TBXML"

---
+++ NongYunTong/PeopleData.h
@@ -0,0 +1,13 @@
+//
+//  PeopleData.h
+//  NongYunTong
+//
+//  Created by YoungShook on 12-5-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PeopleData : NSObject
+
+@end
---
+++ NongYunTong/PeopleData.m
@@ -0,0 +1,13 @@
+//
+//  PeopleData.m
+//  NongYunTong
+//
+//  Created by YoungShook on 12-5-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "PeopleData.h"
+
+@implementation PeopleData
+
+@end
